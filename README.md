cce730336e25e3717be7f68b3eacefe8fb20794b
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-05-20 17:19:42 +0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Sitemap-Reader-
-This code will save sitemap's url into a text file 
+# Sitemap Scrapper
+This code is really helpful if you want to scrap sitemap urls and save them in a file. You just need to install two python libraries, bs4 and requests, and the code will do the rest.
+
+## Install Libraries
+
+```python
+pip install bs4 requests
+```
+
---
+++ Sitemap reader.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup as bs
+from requests import get
+
+url = input("Enter URL: ")
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'referer': 'https://<SCRAPED URL>'
+    }
+
+response = get(url , headers = headers).text
+html = bs(response, 'html.parser')
+allloc= html.findAll('loc')
+allsitemap = []
+
+for loc in allloc:
+    allsitemap.append(loc.text)
+    with open('copy.txt', 'a+') as file:
+        file.writelines(loc.text+'\n')
+        file.close()
